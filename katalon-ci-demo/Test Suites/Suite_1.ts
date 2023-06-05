a9bb4c19ebbbda64f99626aacac9c7ef8e7989bd
son.le <son.le@adminkatalons-MacBook-Pro.local>
2023-06-05 11:20:48 +0700

    Added tests

---
+++ katalon-ci-demo/Test Suites/Suite_1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>54b43fd3-9296-4c6f-9ebb-490988927b9e</testSuiteGuid>
+   <testCaseLink>
+      <guid>d73a3ee2-c79c-4b13-bf73-599934c88770</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
